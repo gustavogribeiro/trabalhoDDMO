829c980b1712034ac8c5103a8ce3069ba63fc6ec
gustavoribeiro <gustavogonzagaribeiro@gmail.com>
2014-12-07 17:34:03 -0200

    listView

---
+++ app/src/main/java/controladorgastos/ddmo/com/controladordegastos/ListaGastos.java
@@ -0,0 +1,7 @@
+package controladorgastos.ddmo.com.controladordegastos;
+
+/**
+ * Created by Gustavo on 12/7/2014.
+ */
+public class ListaGastos {
+}
--- app/src/main/java/controladorgastos/ddmo/com/controladordegastos/MainActivity.java
+++ app/src/main/java/controladorgastos/ddmo/com/controladordegastos/MainActivity.java
@@ -2,16 +2,69 @@ package controladorgastos.ddmo.com.controladordegastos;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TabHost;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText valorGasto, dataGasto;
+    private Spinner categorias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        valorGasto = (EditText) findViewById(R.id.despesa);
+        dataGasto = (EditText) findViewById(R.id.despesaData);
+        categorias = (Spinner) findViewById(R.id.categoria);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost2);
+
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("gerarDespesa");
+        tabSpec.setContent(R.id.gastoAdicionar);
+        tabSpec.setIndicator("Cadastrar gastos");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("consultarDespesa");
+        tabSpec.setContent(R.id.gastoMostrar);
+        tabSpec.setIndicator("Consultar Gastos");
+        tabHost.addTab(tabSpec);
+
+
+        final Button salvarGasto = (Button) findViewById(R.id.salvarGasto);
+        salvarGasto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Despesa salva com sucesso!", Toast.LENGTH_SHORT);
+            }
+        });
+
+        valorGasto.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                salvarGasto.setEnabled(!valorGasto.getText().toString().trim().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
 
